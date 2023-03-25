5b82edb5556535702f9215b0a220c5791facb5a1
안정후 <austinan@naver.com>
2023-03-25 14:38:14 +0900

    project init

---
+++ src/main/java/com/bravebe/bravebe/BraveBeApplication.java
@@ -0,0 +1,13 @@
+package com.bravebe.bravebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraveBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraveBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bravebe/bravebe/BraveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravebe.bravebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraveBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
